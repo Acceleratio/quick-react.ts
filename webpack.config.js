8e4af17f71ad9bd9fd09e33382aebdd4272bd612
Robert Ivišić <robert.ivisic@acceleratio.hr>
2018-01-10 18:31:47 +0100

    New svg icon system

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
         inline: true,
         port: 3000
     },
-    devtool: 'source-map',
+    devtool: 'cheap-module-eval-source-map',
     resolve: {
         extensions: [
             '',
@@ -85,6 +85,7 @@ module.exports = {
             loaders.css,
             loaders.json,
             loaders.svg,
+            loaders.svgIcon,
             loaders.eot,
             loaders.woff,
             loaders.woff2,
