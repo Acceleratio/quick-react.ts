9eb42c65529f7e79f501718305f12b8121cf457a
vinkobedek <vinko.bedek@acceleratio.hr>
2018-01-18 14:17:34 +0100

    removed alert

--- examples/MockData/gridData.ts
+++ examples/MockData/gridData.ts
@@ -63,7 +63,7 @@ const generateTreeData = (size: number): TreeNode => {
         }
         result.push(treeEntry);
     }
-    alert(totalItems);
+    // alert(totalItems);
     return {
         treeId: '',
         parentId: '',
