cdf37e8480eb2655b6d7fa2b7a6ea26ba10456d9
Domagoj Hrestak <domagoj.hrestak@acceleratio.net>
2018-07-02 15:30:38 +0200

    added support for icons with text in pivot title

--- src/components/Pivot/PivotItem.Props.ts
+++ src/components/Pivot/PivotItem.Props.ts
@@ -1,8 +1,14 @@
 import * as React from 'react';
 
+export enum renderModeEnum {
+    Text,
+    Icon,
+    Both
+}
 export interface IPivotItemProps extends React.HTMLProps<HTMLDivElement> {
     linkText: string;
     linkIcon?: string;
+    linkRenderMode?: renderModeEnum;
     itemKey?: string;
     itemCount?: number;
     disabled?: boolean;
