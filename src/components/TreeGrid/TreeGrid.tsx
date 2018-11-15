64a2b155c78e0979593ba3740604bef79b7a4bb7
Tomislav Maslač <tomislav.maslac@syskit.com>
2018-11-15 12:26:56 +0100

    recalculate width when prop changes

--- src/components/TreeGrid/TreeGrid.tsx
+++ src/components/TreeGrid/TreeGrid.tsx
@@ -114,9 +114,11 @@ export class TreeGrid extends React.PureComponent<ITreeGridProps, ITreeGridState
         const result = getTreeRowsSelector(nextState, nextProps, this.props);
         this._finalGridRows = result.data;
         this._maxExpandedLevel = result.maxExpandedLevel;
-        this._quickGrid.updateColumnWidth(0, () => {
-            return this.props.isMultiSelectable ? 35 : 16;
-        });
+        if (this.props.isMultiSelectable !== nextProps.isMultiSelectable) {
+            this._quickGrid.updateColumnWidth(0, () => {
+                return this.props.isMultiSelectable ? 35 : 16;
+            });
+        }
         this._quickGrid.updateColumnWidth(1, (old) => {
             return Math.max(old, getColumnMinWidth(this.state.columnsToDisplay[1]));
         });
