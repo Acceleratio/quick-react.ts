21f33d0232a1acf71b8c30dc2d7f6614395f9077
Matija Maric <matija.maric@syskit.com>
2019-01-18 14:08:29 +0100

    Fixed sorting groupBys

--- src/utilities/resolveCellValue.ts
+++ src/utilities/resolveCellValue.ts
@@ -1,8 +1,12 @@
 import { GridColumn } from '../components/QuickGrid';
 
 export let resolveCellValue = (rowData: any, cell: GridColumn) => {
-    return cell.getCellValue !== undefined ?
+    const value = cell.getCellValue !== undefined ? 
         cell.getCellValue(rowData) : rowData[cell.valueMember];
+    if (typeof value === 'string') {
+        return value.toLowerCase();
+    }
+    return value;
 };
 
 export let resolveCellValueForDisplay = (rowData: any, cell: GridColumn) => {
